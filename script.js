b177e9f3337c996463b4904232145f53b64009e0
CasamentoMeM-arch <alexandramellissafarias@gmail.com>
2025-08-18 22:28:58 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const dataCasamento = new Date("Aug 10, 2025 15:00:00").getTime();
+
+setInterval(() => {
+  const agora = new Date().getTime();
+  const distancia = dataCasamento - agora;
+
+  const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+
+  document.getElementById("contador").innerHTML =
+    dias + " dias " + horas + "h " + minutos + "m restantes";
+}, 1000);
